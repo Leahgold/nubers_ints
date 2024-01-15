85cba48813357fe6eb8b22d98a24e23ceef1a1e3
Leah Gold <ira.bearn@gmail.com>
2024-01-15 15:56:35 +0200

    я молодец

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] Numbers) {
+        int a1 = 8;
+        int a2 = 8;
+        int a3 = 8;
+        int a4 = 8;
+        int result1 = a1 + a2;
+        int result2 = a1 + a2 - a3;
+        int result3 = a1 * a2;
+        int result4 = result3 / a1;
+
+        System.out.println("result1=" + result1);
+        System.out.println("result4=" + result4);
+        System.out.println("result2=" + result2);
+
+
+    }
+}
